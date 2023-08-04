9640898b2d89bdb45842b41a4cfea58af5068bb9
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2023-08-04 10:10:51 +0530

    switch to mongodb

--- README.md
+++ README.md
@@ -3,9 +3,3 @@
 ## Starting the project
 
 Open the project in DevContainer.
-
-To apply migrations, open a new terminal and run:
-
-```
-npm run db:migrate
-```
--- prisma/migrations/20230702151200_add_email_change_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "emailChange" TEXT,
-ADD COLUMN     "emailChangeSentAt" TIMESTAMP(3),
-ADD COLUMN     "emailChangeToken" TEXT;
