c1dc71685c27bb34521642d048c6eef2838c16a0
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2023-08-06 17:22:07 +0530

    remove ws WebSocket from the browser

--- src/utils/centrifugo.ts
+++ src/utils/centrifugo.ts
@@ -1,7 +1,4 @@
 import { Centrifuge } from "centrifuge";
-import WebSocket from "ws";
 import { env } from "~/env.mjs";
 
-export const centrifuge = new Centrifuge(env.NEXT_PUBLIC_CENTRIFUGO_URL, {
-  websocket: WebSocket,
-});
+export const centrifuge = new Centrifuge(env.NEXT_PUBLIC_CENTRIFUGO_URL);
