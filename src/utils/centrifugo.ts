9ccff9a671150e8d0144640edf4c079d5e12dc1f
Aryan Iyappan <69184573+aryan340@users.noreply.github.com>
2023-08-11 09:22:20 +0530

    remove centrifuge console.log statements

--- src/utils/centrifugo.ts
+++ src/utils/centrifugo.ts
@@ -3,14 +3,6 @@ import { env } from "~/env.mjs";
 
 export const centrifuge = new Centrifuge(env.NEXT_PUBLIC_CENTRIFUGO_URL);
 
-centrifuge.on("connected", function () {
-  console.log("Connected to Centrifugo!");
-});
-
-centrifuge.on("disconnected", function (reason) {
-  console.error("Disconnected from Centrifugo:", reason);
-});
-
 centrifuge.on("error", function (error) {
   console.error("Error in Centrifugo:", error);
 });
